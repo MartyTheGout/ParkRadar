2a0f1e0e0b310d35ac59148a283955f5c9d69bb1
MartyTheGout <chihyung0@naver.com>
2025-03-31 20:48:36 +0900

    [Data] Model + Object + DTO completed for SafeParkingArea, NoParkingArea

--- ParkRadar/Data/Realm/Object/NoParkingArea.swift
+++ ParkRadar/Data/Realm/Object/NoParkingArea.swift
@@ -8,19 +8,154 @@
 import Foundation
 import RealmSwift
 
-//    "FIX_CCTV_ADDR": "개포동 1231", // 감시카메라 주소
-//    "LAT": "37.47867",
-//    "LOT": "127.04732",
-//    "CGG_CD": "강남구", // 자치구
-//    "CRDN_BRNCH_NM": "[개포4-102] KB(개포남지점) 주변",
-//    "GRNDS_SE": "불법주정차구역" // not required.
+/**
+ Extenal Data Example :
+ 
+ "FIX_CCTV_ADDR": "개포동 1231",                                      // 감시카메라 주소
+ "LAT": "37.47867",
+ "LOT": "127.04732",
+ "CGG_CD": "강남구",                                                             // 자치구
+ "CRDN_BRNCH_NM": "[개포4-102] KB(개포남지점) 주변",     // 카메라 설명
+ "GRNDS_SE": "불법주정차구역"                                              // 생략
+ 
+ **/
 
 class NoParkingArea : Object {
-    @Persisted var address : String
-//    @Persisted(indexed: true) var latitude : Double
-//    @Persisted(indexed: true) var longitude : Double
+    @Persisted(primaryKey: true) var id: ObjectId
     
+    @Persisted var address: String             // 감시카메라 주소 (FIX_CCTV_ADDR)
+    @Persisted var latitude: Double            // 위도 (LAT)
+    @Persisted var longitude: Double           // 경도 (LOT)
+    @Persisted var district: String            // 자치구 (CGG_CD)
+    @Persisted var descriptionText: String     // 카메라 설명 (CRDN_BRNCH_NM)
     
-}
+    // Indice for Zoom-in Situation Searching
+    @Persisted(indexed: true) var latInt: Int
+    @Persisted(indexed: true) var lngInt: Int
+    
+    // For Clustering
+    @Persisted(indexed: true) var geohash: String
+    
+    convenience init(
+        address: String,
+        latitude: Double,
+        longitude: Double,
+        district: String,
+        descriptionText: String
+    ) {
+        self.init()
+        self.address = address
+        self.latitude = latitude
+        self.longitude = longitude
+        self.district = district
+        self.descriptionText = descriptionText
         
+        self.latInt = Int((latitude * 1000).rounded())
+        self.lngInt = Int((longitude * 1000).rounded())
+        
+        self.geohash = Geohash.encode(latitude: latitude, longitude: longitude, length: 5)
+    }
+    
+    func prepareIndexing() {
+        latInt = Int((latitude * 1000).rounded())
+        lngInt = Int((longitude * 1000).rounded())
+        geohash = Geohash.encode(latitude: latitude, longitude: longitude, length: 5)
+    }
+}
 
+struct NoParkingAreaDTO: Codable {
+    let address: String
+    let latitudeRaw: StringOrDouble
+    let longitudeRaw: StringOrDouble
+    
+    var latitude: Double { latitudeRaw.value ?? 0 }
+    var longitude: Double { longitudeRaw.value ?? 0 }
+    let district: String
+    let descriptionText: String
+    
+    enum CodingKeys: String, CodingKey {
+        case address = "FIX_CCTV_ADDR"
+        case latitudeRaw = "LAT"
+        case longitudeRaw = "LOT"
+        case district = "CGG_CD"
+        case descriptionText = "CRDN_BRNCH_NM"
+    }
+    
+    func toRealmObject() -> NoParkingArea {
+        let obj = NoParkingArea()
+        obj.address = address
+        obj.latitude = latitude
+        obj.longitude = longitude
+        obj.district = district
+        obj.descriptionText = descriptionText
+        obj.prepareIndexing()
+        return obj
+    }
+}
+
+//NoParking데이터의 경우, JSON구조는 동일하지만, 파일 내부의 최상위 키가 계속 달라지는 상황 -> Dynamic Key 
+struct NoParkingAreaResponse: Decodable {
+    let rows: [NoParkingAreaDTO]
+
+    struct DynamicCodingKeys: CodingKey {
+        var stringValue: String
+        init?(stringValue: String) { self.stringValue = stringValue }
+
+        var intValue: Int? { nil }
+        init?(intValue: Int) { return nil }
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: DynamicCodingKeys.self)
+        guard let firstKey = container.allKeys.first else {
+            throw DecodingError.dataCorrupted(.init(codingPath: [],
+                debugDescription: "No top-level key found"))
+        }
+
+        let nestedContainer = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: firstKey)
+        self.rows = try nestedContainer.decode([NoParkingAreaDTO].self, forKey: .row)
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case row
+    }
+}
+
+
+enum StringOrDouble: Codable {
+    case string(String)
+    case double(Double)
+    
+    var value: Double? {
+        switch self {
+        case .string(let str):
+            return Double(str)
+        case .double(let dbl):
+            return dbl
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let dbl = try? container.decode(Double.self) {
+            self = .double(dbl)
+        } else if let str = try? container.decode(String.self) {
+            self = .string(str)
+        } else {
+            throw DecodingError.typeMismatch(
+                StringOrDouble.self,
+                DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Expected Double or String")
+            )
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .double(let dbl):
+            try container.encode(dbl)
+        case .string(let str):
+            try container.encode(str)
+        }
+    }
+}
--- ParkRadar/Data/Realm/Object/SafeParkingArea.swift
+++ ParkRadar/Data/Realm/Object/SafeParkingArea.swift
@@ -6,44 +6,195 @@
 //
 
 import Foundation
+import RealmSwift
+
+/**
+ Extenal Data Example :
+ 
+ "PKLT_NM": "초안산근린공원주차장(구)",  // 이름
+ "ADDR": "도봉구 창동 24-0", // 주소
+ "PKLT_CD": "1010089", // 주차장 넘
+ "PKLT_KND": "NW",
+ "PKLT_KND_NM": "노외 주차장", // 주차장 종류
+ "OPER_SE": "1",
+ "OPER_SE_NM": "시간제 주차장", // 주차장 운영 종류
+ "TELNO": "",
+ "PRK_NOW_INFO_PVSN_YN": "0", // 데이터 연계 여부
+ "PRK_NOW_INFO_PVSN_YN_NM": "미연계중",
+ "TPKCT": 71,
+ "CHGD_FREE_SE": "Y", // 유무료
+ "CHGD_FREE_NM": "유료",
+ "NGHT_FREE_OPN_YN": "N", // 야간 개방 여부
+ "NGHT_FREE_OPN_YN_NAME": "야간 미개방",
+ "WD_OPER_BGNG_TM": "0900", // 평일시작
+ "WD_OPER_END_TM": "1900", // 평일 끝
+ "WE_OPER_BGNG_TM": "0900", // 평일 시작
+ "WE_OPER_END_TM": "1900", // 평일 끝
+ "LHLDY_BGNG": "0900", // 공휴일 시작
+ "LHLDY": "1900", // 공휴일 끝
+ "LAST_DATA_SYNC_TM": "2022-01-12 15:16:35",
+ "SAT_CHGD_FREE_SE": "N",
+ "SAT_CHGD_FREE_NM": "무료", // 토요일 무료
+ "LHLDY_YN": "N",
+ "LHLDY_NM": "무료", // 공휴일 무료
+ "MNTL_CMUT_CRG": "0",
+ "CRB_PKLT_MNG_GROUP_NO": "",
+ "PRK_CRG": 0, // 기본주차
+ "PRK_HM": 0,
+ "ADD_CRG": 300, // 추가주차
+ "ADD_UNIT_TM_MNT": 10,
+ "BUS_PRK_CRG": 0,
+ "BUS_PRK_HM": 0,
+ "BUS_PRK_ADD_HM": 0,
+ "BUS_PRK_ADD_CRG": 0,
+ "DLY_MAX_CRG": 0,
+ "LAT": 0, // 위도
+ "LOT": 0 // 경도
+ **/
+final class SafeParkingArea: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    
+    @Persisted var name: String           // 주차장 이름
+    @Persisted var address: String        // 주소
+    @Persisted var code: String           // 주차장 고유 코드
+    
+    @Persisted var placeKind : String     // 노외 주차장 / 실내주차장
+    @Persisted var operationKind : String // 시간제 주차장
+    
+    @Persisted var isPaid: Bool           // 유료 여부
+    @Persisted var isNightFree: Bool      // 야간 개방 여부
+    @Persisted var isHolidayFree: Bool    // 공휴일 무료 여부
+    
+    @Persisted var weekdayStart: String?  // 평일 시작 시간 (ex. "0900")
+    @Persisted var weekdayEnd: String?    // 평일 종료 시간
+    @Persisted var weekendStart: String?  // 주말 시작 시간
+    @Persisted var weekendEnd: String?    // 주말 종료 시간
+    @Persisted var holidayStart: String?  // 공휴일 시작 시간
+    @Persisted var holidayEnd: String?    // 공휴일 종료 시간
+    
+    @Persisted var baseCharge: Int?       // 기본 요금
+    @Persisted var baseTime: Int?         // 기본 시간 (분 단위)
+    @Persisted var extraCharge: Int?      // 추가 요금
+    @Persisted var extraUnitMinutes: Int? // 추가 요금 단위 시간 (분)
+    @Persisted var dailyMaxCharge: Int?   // 1일 최대 요금
+    
+    // Coordinate
+    @Persisted var latitude: Double?
+    @Persisted var longitude: Double?
+    
+    @Persisted(indexed: true) var latIndex: Int?  // for index based searching
+    @Persisted(indexed: true) var lngIndex: Int?
+    
+    @Persisted var geohash: String? // for clustering
+}
+
+extension SafeParkingArea {
+    func prepareIndexing() {
+        guard let lat = latitude, let lng = longitude else { return }
+        latIndex = Int(lat * 1_000)
+        lngIndex = Int(lng * 1_000)
+        geohash = Geohash.encode(latitude: lat, longitude: lng, length: 5)
+    }
+}
 
 
-//"PKLT_NM": "초안산근린공원주차장(구)",  // 이름
-//"ADDR": "도봉구 창동 24-0", // 주소
-//"PKLT_CD": "1010089", // 주차장 넘
-//"PKLT_KND": "NW",
-//"PKLT_KND_NM": "노외 주차장", // 주차장 종류
-//"OPER_SE": "1",
-//"OPER_SE_NM": "시간제 주차장", // 주차장 운영 종류
-//"TELNO": "",
-//"PRK_NOW_INFO_PVSN_YN": "0", // 데이터 연계 여부
-//"PRK_NOW_INFO_PVSN_YN_NM": "미연계중",
-//"TPKCT": 71,
-//"CHGD_FREE_SE": "Y", // 유무료
-//"CHGD_FREE_NM": "유료",
-//"NGHT_FREE_OPN_YN": "N", // 야간 개방 여부
-//"NGHT_FREE_OPN_YN_NAME": "야간 미개방",
-//"WD_OPER_BGNG_TM": "0900", // 평일시작
-//"WD_OPER_END_TM": "1900", // 평일 끝
-//"WE_OPER_BGNG_TM": "0900", // 평일 시작
-//"WE_OPER_END_TM": "1900", // 평일 끝
-//"LHLDY_BGNG": "0900", // 공휴일 시작
-//"LHLDY": "1900", // 공휴일 끝
-//"LAST_DATA_SYNC_TM": "2022-01-12 15:16:35",
-//"SAT_CHGD_FREE_SE": "N",
-//"SAT_CHGD_FREE_NM": "무료", // 토요일 무료
-//"LHLDY_YN": "N",
-//"LHLDY_NM": "무료", // 공휴일 무료
-//"MNTL_CMUT_CRG": "0",
-//"CRB_PKLT_MNG_GROUP_NO": "",
-//"PRK_CRG": 0, // 기본주차
-//"PRK_HM": 0,
-//"ADD_CRG": 300, // 추가주차
-//"ADD_UNIT_TM_MNT": 10,
-//"BUS_PRK_CRG": 0,
-//"BUS_PRK_HM": 0,
-//"BUS_PRK_ADD_HM": 0,
-//"BUS_PRK_ADD_CRG": 0,
-//"DLY_MAX_CRG": 0,
-//"LAT": 0, // 위도
-//"LOT": 0 // 경도 
+struct SafeParkingAreaDTO: Codable {
+    let name: String
+    let address: String
+    let code: String
+    
+    let placeKind: String
+    let operationKind: String
+    
+    let isPaid: String
+    let isNightFree: String
+    let isHolidayFree: String
+    
+    let weekdayStart: String?
+    let weekdayEnd: String?
+    let weekendStart: String?
+    let weekendEnd: String?
+    let holidayStart: String?
+    let holidayEnd: String?
+    
+    let baseCharge: Int?
+    let baseTime: Int?
+    let extraCharge: Int?
+    let extraUnitMinutes: Int?
+    let dailyMaxCharge: Int?
+    
+    let latitude: Double?
+    let longitude: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "PKLT_NM"
+        case address = "ADDR"
+        case code = "PKLT_CD"
+        
+        case placeKind = "PKLT_KND_NM"
+        case operationKind = "OPER_SE_NM"
+        
+        case isPaid = "CHGD_FREE_SE"
+        case isNightFree = "NGHT_FREE_OPN_YN"
+        case isHolidayFree = "LHLDY_YN"
+        
+        case weekdayStart = "WD_OPER_BGNG_TM"
+        case weekdayEnd = "WD_OPER_END_TM"
+        case weekendStart = "WE_OPER_BGNG_TM"
+        case weekendEnd = "WE_OPER_END_TM"
+        case holidayStart = "LHLDY_BGNG"
+        case holidayEnd = "LHLDY"
+        
+        case baseCharge = "PRK_CRG"
+        case baseTime = "PRK_HM"
+        case extraCharge = "ADD_CRG"
+        case extraUnitMinutes = "ADD_UNIT_TM_MNT"
+        case dailyMaxCharge = "DLY_MAX_CRG"
+        
+        case latitude = "LAT"
+        case longitude = "LOT"
+    }
+    
+    func toRealmObject() -> SafeParkingArea {
+        let obj = SafeParkingArea()
+        obj.name = name
+        obj.address = address
+        obj.code = code
+        
+        obj.placeKind = placeKind
+        obj.operationKind = operationKind
+        
+        obj.isPaid = (isPaid == "Y")
+        obj.isNightFree = (isNightFree == "Y")
+        obj.isHolidayFree = (isHolidayFree == "Y")
+        
+        obj.weekdayStart = weekdayStart
+        obj.weekdayEnd = weekdayEnd
+        obj.weekendStart = weekendStart
+        obj.weekendEnd = weekendEnd
+        obj.holidayStart = holidayStart
+        obj.holidayEnd = holidayEnd
+        
+        obj.baseCharge = baseCharge
+        obj.baseTime = baseTime
+        obj.extraCharge = extraCharge
+        obj.extraUnitMinutes = extraUnitMinutes
+        obj.dailyMaxCharge = dailyMaxCharge
+        
+        obj.latitude = latitude
+        obj.longitude = longitude
+        
+        obj.prepareIndexing()
+        
+        return obj
+    }
+}
+
+struct SafeParkingResponse: Codable {
+    let GetParkInfo: SafeParkingWrapper
+}
+
+struct SafeParkingWrapper: Codable {
+    let list_total_count: Int
+    let row: [SafeParkingAreaDTO]
+}
