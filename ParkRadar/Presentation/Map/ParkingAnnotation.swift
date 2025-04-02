74da92e28bad78f160db35aede4c72a1ccb32a51
MartyTheGout <chihyung0@naver.com>
2025-04-02 16:21:17 +0900

    [Design] Customize annotation symbol with designated sf symbol

--- ParkRadar/Presentation/Map/ParkingAnnotation.swift
+++ ParkRadar/Presentation/Map/ParkingAnnotation.swift
@@ -11,11 +11,13 @@ final class SafeAnnotation: NSObject, MKAnnotation {
     let coordinate: CLLocationCoordinate2D
     let title: String?
     let subtitle: String?
+    let symbolName: String
 
     init(from model: SafeParkingArea) {
         self.coordinate = CLLocationCoordinate2D(latitude: model.latitude ?? 0, longitude: model.longitude ?? 0)
         self.title = model.name
         self.subtitle = model.address
+        self.symbolName = "car.2.fill"
     }
 }
 
@@ -23,10 +25,12 @@ final class DangerAnnotation: NSObject, MKAnnotation {
     let coordinate: CLLocationCoordinate2D
     let title: String?
     let subtitle: String?
+    let symbolName: String
 
     init(from model: NoParkingArea) {
         self.coordinate = CLLocationCoordinate2D(latitude: model.latitude, longitude: model.longitude)
         self.title = model.district
         self.subtitle = model.descriptionText
+        self.symbolName = "eye.fill"
     }
 }
