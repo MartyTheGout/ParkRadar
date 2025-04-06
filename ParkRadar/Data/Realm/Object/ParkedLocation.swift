ccc8293fa041050ad96de752dec146e0298b4862
MartyTheGout <chihyung0@naver.com>
2025-04-06 23:39:40 +0900

    [Data] Update Model, ParkedLocation

--- ParkRadar/Data/Realm/Object/ParkedLocation.swift
+++ ParkRadar/Data/Realm/Object/ParkedLocation.swift
@@ -9,18 +9,20 @@ import Foundation
 import RealmSwift
 
 final class ParkedLocation: Object {
-    @Persisted var id: ObjectId
+    @Persisted(primaryKey: true) var id: ObjectId
     @Persisted var latitude: Double
     @Persisted var longitude: Double
+    @Persisted var title: String
     @Persisted(indexed: true) var latInt: Int
     @Persisted(indexed: true) var lngInt: Int
     @Persisted var imagePath: String?
     @Persisted var savedDate: Date
     
-    convenience init(latitude: Double, longitude: Double, imagePath: String?, date: Date = Date()) {
+    convenience init(latitude: Double, longitude: Double, title: String, imagePath: String?, date: Date = Date()) {
         self.init()
         self.latitude = latitude
         self.longitude = longitude
+        self.title = title
         self.imagePath = imagePath
         self.savedDate = date
         
@@ -28,5 +30,3 @@ final class ParkedLocation: Object {
         self.lngInt = Int((longitude * 1000).rounded())
     }
 }
-
-
