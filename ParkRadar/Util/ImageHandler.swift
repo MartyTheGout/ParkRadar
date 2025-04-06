6632cb68862b47db607637cac937fc544ae24302
MartyTheGout <chihyung0@naver.com>
2025-04-06 23:40:20 +0900

    [Util] add ImageHandler

---
+++ ParkRadar/Util/ImageHandler.swift
@@ -0,0 +1,55 @@
+//
+//  ImageHandler.swift
+//  ParkRadar
+//
+//  Created by marty.academy on 4/6/25.
+//
+
+import UIKit
+
+class ImageHandler {
+    func saveImageToDocument(image: UIImage, fileName: String) {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
+        
+        let fileURL = documentDirectory.appendingPathComponent("\(fileName).jpg")
+        removeImageFromDocument(filename: fileName)
+        
+        guard let data = image.jpegData(compressionQuality: 0.5) else { return }
+        
+        do {
+            try data.write(to: fileURL)
+        } catch {
+            print("[Error] Failed to save image to document directory.")
+        }
+    }
+    
+    func loadImageFromDocument(filename: String) -> UIImage? {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        
+        let fileURL = documentDirectory.appendingPathComponent("\(filename).jpg")
+        
+        if FileManager.default.fileExists(atPath: fileURL.path()) {
+            return UIImage(contentsOfFile: fileURL.path())
+        } else {
+            return UIImage(systemName: "pin.fill")
+        }
+    }
+    
+    func removeImageFromDocument(filename: String) {
+        guard let documentDirectory = FileManager.default.urls(
+            for: .documentDirectory,
+            in: .userDomainMask).first else { return }
+        
+        let fileURL = documentDirectory.appendingPathComponent("\(filename).jpg")
+        
+        if FileManager.default.fileExists(atPath: fileURL.path()) {
+            do {
+                try FileManager.default.removeItem(atPath: fileURL.path())
+            } catch {
+                print("[Error] File remove error", error)
+            }
+        } else {
+            print("[Error] File no exist")
+        }
+    }
+}
