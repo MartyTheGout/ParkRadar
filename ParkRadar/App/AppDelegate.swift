62ec4004e0a063cf6d33f8caf3f3b1115ea0df3d
MartyTheGout <chihyung0@naver.com>
2025-04-09 16:09:30 +0900

    [Monitoring] Firebase Crashlytics Installation

--- ParkRadar/App/AppDelegate.swift
+++ ParkRadar/App/AppDelegate.swift
@@ -7,16 +7,22 @@
 
 import UIKit
 import KakaoSDKCommon
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        //KakaoSDK Setting
         KakaoSDK.initSDK(appKey: AuthorizationManager.app.appKey ?? "")
         
+        //InitialData Setting
         RecordLoader.loadNoParkingDataIfNeeded()
         RecordLoader.loadSafeParkingAreaIfNeeded()
+        
+        //FirebaseSDK Setting
+        FirebaseApp.configure()
 
         return true
     }
@@ -34,7 +40,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
+
+
 
