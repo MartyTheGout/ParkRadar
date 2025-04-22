423c33e710ba8c8b479d1a5e2ae22f29287e3353
MartyTheGout <chihyung0@naver.com>
2025-04-22 21:37:21 +0900

    [Extension] Widget added

---
+++ ParkRadarWidget/ParkRadarWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ParkRadarWidgetBundle.swift
+//  ParkRadarWidget
+//
+//  Created by marty.academy on 4/22/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ParkRadarWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ParkRadarWidget()
+    }
+}
